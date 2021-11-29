7f2f1ef1d93149ea9fb75b89c51825480926056b
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-11-29 16:29:37 -0600

    Tarea con inputs y módulos

---
+++ 01-bases/src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+    selector: 'app-agregar',
+    templateUrl: './agregar.component.html',
+    styleUrls: ['./agregar.component.css'],
+})
+export class AgregarComponent {
+    @Input() nuevo: Personaje = { nombre: '', poder: 0 };
+    @Input() personajes: Personaje[] = [];
+
+    agregar() {
+        if (this.nuevo.nombre.trim().length === 0) {
+            return;
+        }
+
+        this.personajes.push(this.nuevo);
+        console.log(this.personajes);
+        this.nuevo = {
+            nombre: '',
+            poder: 0,
+        };
+    }
+}
