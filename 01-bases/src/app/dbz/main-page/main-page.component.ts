68b4c3648812d1ca124064aac4a0945ad74cb3ec
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-11-29 14:07:45 -0600

    Mostrar listado de personajes

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -11,11 +11,32 @@ interface Personaje {
     styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+    personajes: Personaje[] = [
+        {
+            nombre: 'Goku',
+            poder: 15000,
+        },
+        {
+            nombre: 'Vegueta',
+            poder: 7500,
+        },
+    ];
+
     nuevo: Personaje = {
-        nombre: 'Trunks',
-        poder: 14000,
+        nombre: '',
+        poder: 0,
     };
     agregar() {
+        if (this.nuevo.nombre.trim().length === 0) {
+            return;
+        }
+
         console.log(this.nuevo);
+
+        this.personajes.push(this.nuevo);
+        this.nuevo = {
+            nombre: '',
+            poder: 0,
+        };
     }
 }
