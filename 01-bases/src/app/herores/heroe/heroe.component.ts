d86392ddd66383637a906a5adb40f9887f3c3e24
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-11-28 21:13:27 -0600

    Concepto de one way data binding - enlazado en una sola vía 6

--- 01-bases/src/app/herores/heroe/heroe.component.ts
+++ 01-bases/src/app/herores/heroe/heroe.component.ts
@@ -15,4 +15,11 @@ export class HeroreComponent {
     obtenerNombre(): string {
         return `${this.nombre} - ${this.edad} `;
     }
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 30;
+    }
 }
