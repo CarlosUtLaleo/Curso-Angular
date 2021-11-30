06db35617c55a62d4aa350568de1a584e1a7b9b5
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-11-30 14:25:44 -0600

    Colocando un tipado a las peticiones http

--- 02-gifsApp/src/app/gifs/resultados/resultados.component.ts
+++ 02-gifsApp/src/app/gifs/resultados/resultados.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Gif } from '../interface/gifs.interface';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
-  selector: 'app-resultados',
-  templateUrl: './resultados.component.html',
-  styles: [
-  ]
+    selector: 'app-resultados',
+    templateUrl: './resultados.component.html',
+    styles: [],
 })
-export class ResultadosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ResultadosComponent {
+    get resultados(): Gif[] {
+        return this.gifsService.resultados;
+    }
+    constructor(private gifsService: GifsService) {}
 }
--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
     providedIn: 'root',
@@ -7,6 +8,9 @@ import { Injectable } from '@angular/core';
 export class GifsService {
     private _historial: string[] = [];
     private apiKey: string = 'Soca8TE6nv54lsyOm1tSjitOuaJ4HTwc';
+
+    // TODO: Cambiar any por el tipo correspondiente
+    public resultados: Gif[] = [];
 
     get historial() {
         return [...this._historial];
@@ -21,9 +25,12 @@ export class GifsService {
             this._historial = this._historial.splice(0, 9);
         }
         this.http
-            .get('https://api.giphy.com/v1/gifs/search?api_key=Soca8TE6nv54lsyOm1tSjitOuaJ4HTwc&q=dragon ball z&limit=10')
-            .subscribe((resp: any) => {
+            .get<SearchGifsResponse>(
+                `https://api.giphy.com/v1/gifs/search?api_key=Soca8TE6nv54lsyOm1tSjitOuaJ4HTwc&q=${query}&limit=10`
+            )
+            .subscribe((resp: SearchGifsResponse) => {
                 console.log(resp.data);
+                this.resultados = resp.data;
             });
     }
 }
