dcb4ce0c0106dcbcb7b5847e4bfb18507c0c1ed4
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-11-30 13:42:48 -0600

    Realizar una petición HTTP

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,16 +6,24 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
     private _historial: string[] = [];
+    private apiKey: string = 'Soca8TE6nv54lsyOm1tSjitOuaJ4HTwc';
 
     get historial() {
         return [...this._historial];
     }
 
+    constructor(private http: HttpClient) {}
+
     buscarGifs(query: string) {
         query = query.trim().toLocaleLowerCase();
         if (!this._historial.includes(query)) {
             this._historial.unshift(query);
-            this._historial = this._historial.splice(0, 10);
+            this._historial = this._historial.splice(0, 9);
         }
+        this.http
+            .get('https://api.giphy.com/v1/gifs/search?api_key=Soca8TE6nv54lsyOm1tSjitOuaJ4HTwc&q=dragon ball z&limit=10')
+            .subscribe((resp: any) => {
+                console.log(resp.data);
+            });
     }
 }
