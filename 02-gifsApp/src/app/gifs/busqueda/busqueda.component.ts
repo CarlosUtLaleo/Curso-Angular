238a255eee5eccd076580b256947561df62e94c2
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-11-30 13:14:04 -0600

    GifsService

--- 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
     selector: 'app-busqueda',
@@ -10,7 +11,9 @@ export class BusquedaComponent {
     buscar() {
         let valor = this.txtBuscar.nativeElement.value;
 
-        console.log(valor);
+        this.gifsService.buscarGifs(valor);
         this.txtBuscar.nativeElement.value = '';
     }
+
+    constructor(private gifsService: GifsService) {}
 }
