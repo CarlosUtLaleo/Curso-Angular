41c2b92b8c8796648dfb08f27518be73da534c70
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-11-30 16:13:23 -0600

    localStorage

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -16,13 +16,18 @@ export class GifsService {
         return [...this._historial];
     }
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+            this._historial = JSON.parse(localStorage.getItem("historial")!)||[]
+        
+    }
 
     buscarGifs(query: string) {
         query = query.trim().toLocaleLowerCase();
         if (!this._historial.includes(query)) {
             this._historial.unshift(query);
             this._historial = this._historial.splice(0, 9);
+
+            localStorage.setItem("historial", JSON.stringify(this._historial))
         }
         this.http
             .get<SearchGifsResponse>(
