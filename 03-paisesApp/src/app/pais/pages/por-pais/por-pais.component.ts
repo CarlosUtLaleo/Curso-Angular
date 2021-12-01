e5abfe98e45b8ae0ae15f2ad0e57ced999bf15f0
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-12-01 14:23:54 -0600

    Llenar la tabla de países

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -10,6 +11,8 @@ export class PorPaisComponent {
     termino: string = '';
     hayError: boolean = false;
 
+    paises: Country[] = [];
+
     constructor(private paisService: PaisService) {}
 
     buscar() {
@@ -18,11 +21,13 @@ export class PorPaisComponent {
         this.paisService.buscarPais(this.termino).subscribe(
             (paises) => {
                 console.log(paises);
+                this.paises = paises;
             },
             (error) => {
                 console.log('Error');
                 console.info(error);
                 this.hayError = true;
+                this.paises = [];
             }
         );
     }
