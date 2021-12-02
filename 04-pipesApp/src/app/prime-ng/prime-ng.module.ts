641a6630fe0a048edc337d9132d734a54aa98b5c
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-12-02 14:33:51 -0600

    PrimeNg - MenuBar

--- 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 //PrimeNg
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @NgModule({
-	exports: [ButtonModule, CardModule],
+	exports: [ButtonModule, CardModule, MenubarModule],
 })
 export class PrimeNgModule {}
