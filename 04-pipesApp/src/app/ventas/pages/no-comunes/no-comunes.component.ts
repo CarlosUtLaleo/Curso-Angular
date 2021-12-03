f77ac24ca72afb879a8a62c03e05452b9ea7e53b
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-12-03 13:40:16 -0600

    I18nSelectPipe

--- 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-no-comunes',
-  templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+	selector: 'app-no-comunes',
+	templateUrl: './no-comunes.component.html',
+	styles: [],
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
+	nombre: string = 'Fernando';
+	genero: string = 'masculino';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	invitacionMap = {
+		masculino: 'invirarlo',
+		femenino: 'invitarla',
+	};
 }
