2c17c6cd5bcb922ee072b08e76aae5b28521c7fd
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-12-03 13:51:29 -0600

    I18nPluralPipe

--- 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
 	styles: [],
 })
 export class NoComunesComponent {
+	//i18nSelect
 	nombre: string = 'Fernando';
 	genero: string = 'masculino';
 
 	invitacionMap = {
 		masculino: 'invirarlo',
 		femenino: 'invitarla',
+	};
+
+	//i18nPlural
+	clientes: string[] = ['Maria', 'Pedro', 'Juan'];
+	clientesMapa = {
+		'=0': 'no tenemos ningun cliente esperando',
+		'=1': 'tenemos 1 cliente esperando',
+		'=2': 'tenemos 2 clientes esperando',
+		other: 'tenemos # clientes esperando',
 	};
 }
