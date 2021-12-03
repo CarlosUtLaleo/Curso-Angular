87e144305fec2aa098e8beb022a23fe2cca40c26
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-12-03 12:57:07 -0600

    DecimalPipe

--- 04-pipesApp/src/app/ventas/pages/numeros/numeros.component.ts
+++ 04-pipesApp/src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-numeros',
-  templateUrl: './numeros.component.html',
-  styles: [
-  ]
+	selector: 'app-numeros',
+	templateUrl: './numeros.component.html',
+	styles: [],
 })
-export class NumerosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NumerosComponent {
+	ventasNetas: number = 1321444.1234;
+	porcentaje: number = 0.48;
 }
