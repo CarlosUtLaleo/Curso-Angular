f125d401240500c1cd4c740c5d0759fb4a143651
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-12-06 11:01:09 -0600

    Tarea pipe personalizado - VuelaPipe

---
+++ 04-pipesApp/src/app/ventas/pipes/vuela.pipe.ts
@@ -0,0 +1,7 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({ name: 'vuela' })
+export class VuelaPipe implements PipeTransform {
+	transform(value: Boolean) {
+		return value ? 'vuela' : 'no vuela';
+	}
+}
