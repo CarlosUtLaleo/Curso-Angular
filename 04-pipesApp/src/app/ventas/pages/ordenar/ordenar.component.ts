1d9211fdf63493f2719352e722b03259c7fd093a
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-12-06 10:40:38 -0600

    Llenar un PrimeTable con data

--- 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
 	selector: 'app-ordenar',
 	templateUrl: './ordenar.component.html',
 	styles: [],
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 	mayusculas: boolean = true;
 
-	cars = [];
-	constructor() {}
-
-	ngOnInit(): void {}
+	heroes: Heroe[] = [
+		{ nombre: 'Superman', vuela: true, color: Color.azul },
+		{ nombre: 'Batman', vuela: false, color: Color.negro },
+		{ nombre: 'Robin', vuela: false, color: Color.verde },
+		{ nombre: 'Darevil', vuela: false, color: Color.negro },
+		{ nombre: 'Linterna verde', vuela: true, color: Color.verde },
+	];
 
 	toggleMayusculas() {
 		this.mayusculas = !this.mayusculas;
