0e086dd5eba51c6c26c694db2611fe2206e82073
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-13 09:07:55 -0600

    Eliminar registros

--- 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -68,4 +68,10 @@ export class AgregarComponent implements OnInit {
 			});
 		}
 	}
+
+	borrar() {
+		this.heroesService.borrarHeroe(this.heroe.id!).subscribe((resp) => {
+			this.router.navigate(['/heroes']);
+		});
+	}
 }
--- 05-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 05-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -5,32 +5,35 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class HeroesService {
-  private baseUrl: string = environment.baseUrl
+	private baseUrl: string = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) {}
 
-  getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(this.baseUrl + "/heroes")
-  }
+	getHeroes(): Observable<Heroe[]> {
+		return this.http.get<Heroe[]>(this.baseUrl + '/heroes');
+	}
 
-  getHeroeById(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(this.baseUrl + "/heroes/" + id)
-  }
+	getHeroeById(id: string): Observable<Heroe> {
+		return this.http.get<Heroe>(this.baseUrl + '/heroes/' + id);
+	}
 
-  getHeroeByName(name: string) : Observable<Heroe[]>{
-    console.log(name)
-    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${name}&_limit=6`)
-  }
+	getHeroeByName(name: string): Observable<Heroe[]> {
+		console.log(name);
+		return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${name}&_limit=6`);
+	}
 
-  agregarHeroe(heroe: Heroe): Observable<Heroe> {
-    return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe)
-  }
+	agregarHeroe(heroe: Heroe): Observable<Heroe> {
+		return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe);
+	}
 
-   actualizarHeroe(heroe: Heroe): Observable<Heroe> {
-    return this.http.put<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`, heroe)
-  }
+	actualizarHeroe(heroe: Heroe): Observable<Heroe> {
+		return this.http.put<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`, heroe);
+	}
 
+	borrarHeroe(id: string) {
+		return this.http.delete<Heroe>(`${this.baseUrl}/heroes/${id}`);
+	}
 }
