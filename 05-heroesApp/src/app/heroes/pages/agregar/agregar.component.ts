7cc2c39248d37739b46fce0f524a25d3a0413765
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-13 10:29:28 -0600

    Adecuar los textos de la pantalla de agregar

--- 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -75,13 +75,19 @@ export class AgregarComponent implements OnInit {
 	}
 
 	borrar() {
-		this.dialog.open(ConfirmarComponent, {
+		const dialog = this.dialog.open(ConfirmarComponent, {
 			width: '250px',
+			data: this.heroe,
 		});
-		/* 		this.heroesService.borrarHeroe(this.heroe.id!).subscribe((resp) => {
-			this.router.navigate(['/heroes']);
-			this.mostrarSnackBar('Registro borrado');
-		}); */
+
+		dialog.afterClosed().subscribe((result) => {
+			if (result) {
+				this.heroesService.borrarHeroe(this.heroe.id!).subscribe((resp) => {
+					this.router.navigate(['/heroes']);
+					this.mostrarSnackBar('Registro borrado');
+				});
+			}
+		});
 	}
 
 	mostrarSnackBar(mensaje: string) {
