01fd079e7015bd34b6dc408d0acbb2c02b48c2a1
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-13 14:31:21 -0600

    Mantener la sesión del usuario

--- 05-heroesApp/src/app/auth/guards/auth.guard.ts
+++ 05-heroesApp/src/app/auth/guards/auth.guard.ts
@@ -8,29 +8,33 @@ import {
 	UrlSegment,
 	UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthGuard implements CanLoad {
-	constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService, private router: Router) {}
 
 	canActivate(
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot
 	): Observable<boolean> | Promise<boolean> | boolean {
-		return true;
+		return this.authService.verificaAutenticacion().pipe(
+			tap((autenticado) => {
+				if (!autenticado) {
+					this.router.navigate(['./auth/login']);
+				}
+			})
+		);
 	}
 	canLoad(
 		route: Route,
 		segments: UrlSegment[]
 	): Observable<boolean> | Promise<boolean> | boolean {
-		if (this.authService.auth.id) {
-			return true;
-		}
-
-		return false;
+		return this.authService.verificaAutenticacion();
 	}
 }
--- 05-heroesApp/src/app/auth/services/auth.service.ts
+++ 05-heroesApp/src/app/auth/services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 import { tap } from 'rxjs/operators';
+import { Observable, of, map } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,6 +11,18 @@ import { tap } from 'rxjs/operators';
 export class AuthService {
 	private baseUrl: string = environment.baseUrl;
 	constructor(private http: HttpClient) {}
+
+	verificaAutenticacion(): Observable<boolean> {
+		if (!localStorage.getItem('id')) {
+			return of(false);
+		}
+		return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`).pipe(
+			map((auth) => {
+				this._auth = auth;
+				return true;
+			})
+		);
+	}
 
 	private _auth: Auth | undefined;
 
