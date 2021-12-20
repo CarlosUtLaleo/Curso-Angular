d339e7bb276b0534ad204cdf1983c4fd9716e76c
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-20 10:08:45 -0600

    SideMenu

--- 06-formulariosApp/src/app/shared/sidemenu/sidemenu.component.ts
+++ 06-formulariosApp/src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+	text: string;
+	ruta: string;
+}
+
 @Component({
-  selector: 'app-sidemenu',
-  templateUrl: './sidemenu.component.html',
-  styleUrls: ['./sidemenu.component.css']
+	selector: 'app-sidemenu',
+	templateUrl: './sidemenu.component.html',
+	styleUrls: ['./sidemenu.component.css'],
 })
 export class SidemenuComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	templateMenu: MenuItem[] = [
+		{
+			text: 'Basicos',
+			ruta: './template/basicos',
+		},
+		{
+			text: 'Dinamicos',
+			ruta: './template/dinamicos',
+		},
+		{
+			text: 'Switches',
+			ruta: './template/switches',
+		},
+	];
 
-  ngOnInit(): void {
-  }
+	reactiveMenu: MenuItem[] = [
+		{
+			text: 'Basicos',
+			ruta: './reactive/basicos',
+		},
+		{
+			text: 'Dinamicos',
+			ruta: './reactive/dinamicos',
+		},
+		{
+			text: 'Switches',
+			ruta: './reactive/switches',
+		},
+	];
 
+	ngOnInit(): void {}
 }
