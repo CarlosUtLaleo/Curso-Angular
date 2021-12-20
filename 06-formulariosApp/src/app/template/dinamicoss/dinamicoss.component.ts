fb651151e9ae327c3ba97f0f022651cab64d98e3
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-20 15:26:32 -0600

    Template: Formularios dinámicos y arreglos

--- 06-formulariosApp/src/app/template/dinamicoss/dinamicoss.component.ts
+++ 06-formulariosApp/src/app/template/dinamicoss/dinamicoss.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dinamicoss',
-  templateUrl: './dinamicoss.component.html',
-  styles: [
-  ]
+	selector: 'app-dinamicoss',
+	templateUrl: './dinamicoss.component.html',
+	styles: [],
 })
 export class DinamicossComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	guardar() {}
 }
