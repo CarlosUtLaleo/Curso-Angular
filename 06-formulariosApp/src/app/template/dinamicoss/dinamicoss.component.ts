58e2aa35074d0ae337133cfea38f71285caeccbc
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-20 15:39:03 -0600

    Agregar elementos de forma dinámica

--- 06-formulariosApp/src/app/template/dinamicoss/dinamicoss.component.ts
+++ 06-formulariosApp/src/app/template/dinamicoss/dinamicoss.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface Persona {
+	nombre: string;
+	favoritos: Favorito[];
+}
+
+interface Favorito {
+	id: number;
+	nombre: string;
+}
 @Component({
 	selector: 'app-dinamicoss',
 	templateUrl: './dinamicoss.component.html',
 	styles: [],
 })
-export class DinamicossComponent implements OnInit {
-	constructor() {}
-
-	ngOnInit(): void {}
+export class DinamicossComponent {
+	persona: Persona = {
+		nombre: 'Carlos',
+		favoritos: [
+			{
+				id: 1,
+				nombre: 'Metal Gear',
+			},
+			{
+				id: 2,
+				nombre: 'DeathStranding',
+			},
+		],
+	};
 
 	guardar() {}
 }
