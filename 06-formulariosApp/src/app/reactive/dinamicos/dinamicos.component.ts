2fece7eb98f70cacb75f3726d9f6cc098147fbdb
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-23 13:13:27 -0600

    Eliminar elementos de un FormArray

--- 06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ 06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'app-dinamicos',
@@ -17,6 +17,9 @@ export class DinamicosComponent implements OnInit {
 			Validators.required
 		),
 	});
+	nuevoFavorito: FormControl = this.fb.control("", Validators.required)
+
+
 	get favoritosArr() {
 		return this.miFormulario.get('favoritos') as FormArray;
 	}
@@ -28,6 +31,16 @@ export class DinamicosComponent implements OnInit {
 		);
 	}
 
+	agregarFavorito() {
+		if (this.nuevoFavorito.invalid) return;
+
+		this.favoritosArr.push(new FormControl(this.nuevoFavorito.value, Validators.required))
+		this.nuevoFavorito.reset("")
+	}
+
+	borrar(i: number) {
+		this.favoritosArr.removeAt(i)
+	}
 	guardar() {
 		if (this.miFormulario.invalid) {
 			this.miFormulario.markAllAsTouched();
