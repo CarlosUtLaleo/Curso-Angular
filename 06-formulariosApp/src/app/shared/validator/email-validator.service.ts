4a5a60f3287a380216c54ef176520fd5c9e1efc2
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-27 11:51:37 -0600

    Estado del formulario

--- 06-formulariosApp/src/app/shared/validator/email-validator.service.ts
+++ 06-formulariosApp/src/app/shared/validator/email-validator.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 @Injectable({
 	providedIn: 'root',
 })
@@ -12,9 +12,11 @@ export class EmailValidatorService implements AsyncValidator {
 		control: AbstractControl
 	): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
 		const email = control.value;
-		console.log(email);
+
 		return this.http.get<any[]>('http://localhost:3000/usuarios?q=' + email).pipe(
+			delay(3000),
 			map((resp) => {
+				console.log(email);
 				return resp.length === 0 ? null : { emailTomado: true };
 			})
 		);
