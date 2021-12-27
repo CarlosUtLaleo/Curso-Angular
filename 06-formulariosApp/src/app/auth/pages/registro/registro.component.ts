0b5798521619b86c5e886a377cfa6f7c1854367e
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-27 10:28:22 -0600

    Validar contraseñas iguales

--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -10,14 +10,21 @@ import { ValidatorService } from '../../../shared/validator/validator.service';
 export class RegistroComponent implements OnInit {
 	//TODO: Temporal
 
-	miFormulario: FormGroup = this.fb.group({
-		nombre: [
-			'',
-			[Validators.required, Validators.pattern(this.vs.nombreApellidoPattern)],
-		],
-		email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
-		username: ['', [Validators.required, this.vs.noPuedeSerStrider]],
-	});
+	miFormulario: FormGroup = this.fb.group(
+		{
+			nombre: [
+				'',
+				[Validators.required, Validators.pattern(this.vs.nombreApellidoPattern)],
+			],
+			email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
+			username: ['', [Validators.required, this.vs.noPuedeSerStrider]],
+			password: ['', [Validators.required, Validators.minLength(6)]],
+			password2: ['', [Validators.required]],
+		},
+		{
+			validators: [this.vs.camposIguales('password', 'password2')],
+		}
+	);
 
 	constructor(private fb: FormBuilder, private vs: ValidatorService) {}
 
