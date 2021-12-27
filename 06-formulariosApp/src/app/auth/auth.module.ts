a0f207fbdfb49ba1687d29112be867aacc0887d7
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-27 09:19:43 -0600

    Validar contra una expresión regular

--- 06-formulariosApp/src/app/auth/auth.module.ts
+++ 06-formulariosApp/src/app/auth/auth.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    RegistroComponent,
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+	declarations: [RegistroComponent, LoginComponent],
+	imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+	selector: 'app-registro',
+	templateUrl: './registro.component.html',
+	styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
+	//TODO: Temporal
 
-  constructor() { }
+	nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
 
-  ngOnInit(): void {
-  }
+	miFormulario: FormGroup = this.fb.group({
+		nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+	});
 
+	constructor(private fb: FormBuilder) {}
+
+	ngOnInit(): void {}
+
+	campoNoValido(campo: string) {
+		return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
+	}
+
+	submitFormulario() {
+		console.log(this.miFormulario.value);
+
+		this.miFormulario.markAllAsTouched();
+	}
 }
