9af48d7deb8881b959b5ab89a3fe12b5bd6b7507
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-29 10:45:49 -0600

    Menú de la aplicación

--- 09-mapasApp/src/app/shared/menu/menu.component.ts
+++ 09-mapasApp/src/app/shared/menu/menu.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+	ruta: string;
+	nombre: string;
+}
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+	selector: 'app-menu',
+	templateUrl: './menu.component.html',
+	styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MenuComponent {
+	menuItems: MenuItem[] = [
+		{
+			ruta: '/mapas/fullscreen',
+			nombre: 'Fullscreen',
+		},
+		{
+			ruta: '/mapas/zoom-range',
+			nombre: 'Zoom Range',
+		},
+		{
+			ruta: '/mapas/marcadores',
+			nombre: 'Marcadores',
+		},
+		{
+			ruta: '/mapas/propiedades',
+			nombre: 'Propiedades',
+		},
+	];
 }
