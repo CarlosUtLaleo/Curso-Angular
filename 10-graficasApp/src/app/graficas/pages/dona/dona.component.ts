ba5d8a4de3e5a3ddd17183398b7208d4af48670c
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-30 13:50:36 -0600

    Gráfica de Dona

--- 10-graficasApp/src/app/graficas/pages/dona/dona.component.ts
+++ 10-graficasApp/src/app/graficas/pages/dona/dona.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
-  selector: 'app-dona',
-  templateUrl: './dona.component.html',
-  styles: [
-  ]
+	selector: 'app-dona',
+	templateUrl: './dona.component.html',
+	styles: [],
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
+	// Doughnut
+	public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+	public doughnutChartData: ChartData<'doughnut'> = {
+		labels: this.doughnutChartLabels,
+		datasets: [{ data: [350, 450, 100] }],
+	};
+	public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
+	// events
+	public chartClicked({ event, active }: { event: MouseEvent; active: {}[] }): void {
+		console.log(event, active);
+	}
 
-  ngOnInit(): void {
-  }
-
+	public chartHovered({ event, active }: { event: MouseEvent; active: {}[] }): void {
+		console.log(event, active);
+	}
 }
