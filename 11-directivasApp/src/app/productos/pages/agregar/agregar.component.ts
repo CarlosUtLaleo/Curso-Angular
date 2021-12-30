2b113e0889cd3af12ee31b43f346d06a6840519b
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-30 17:46:05 -0600

    Directivas estructurales personalizadas

--- 11-directivasApp/src/app/productos/pages/agregar/agregar.component.ts
+++ 11-directivasApp/src/app/productos/pages/agregar/agregar.component.ts
@@ -7,21 +7,25 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 	styles: [],
 })
 export class AgregarComponent {
+	texto1: string = 'Fernando Herrera';
+	color: string = 'green';
+
 	miFormulario: FormGroup = this.fb.group({
 		nombre: ['', Validators.required],
 	});
+
 	constructor(private fb: FormBuilder) {}
 
-	texto1: string = 'Debe ingresar este campo';
-	color: string = ' red';
-	cambiarTexto() {
-		this.texto1 = 'Juan Carlos';
+	tieneError(campo: string): boolean {
+		return this.miFormulario.get(campo)?.invalid || false;
 	}
+
+	cambiarNombre() {
+		this.texto1 = Math.random().toString();
+	}
+
 	cambiarColor() {
 		const color = '#xxxxxx'.replace(/x/g, (y) => ((Math.random() * 16) | 0).toString(16));
 		this.color = color;
-	}
-	tieneError(campo: string) {
-		return this.miFormulario.get(campo)?.invalid || false;
 	}
 }
