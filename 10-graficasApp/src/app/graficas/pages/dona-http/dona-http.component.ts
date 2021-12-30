85e99b42967d28408f40907db49b06fc7a9a8bda
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-30 14:06:53 -0600

    Mostrar la información en la gráfica

--- 10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
+++ 10-graficasApp/src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GraficasService } from '../../services/graficas.service';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
 	selector: 'app-dona-http',
@@ -12,6 +13,26 @@ export class DonaHttpComponent implements OnInit {
 	ngOnInit(): void {
 		this.graficasService.getUsuariosRedesSociales().subscribe((data) => {
 			console.log(data);
+			const labels = Object.keys(data);
+			const value = Object.values(data);
+
+			this.doughnutChartLabels = labels;
+			this.doughnutChartData.datasets.push({ data: value });
 		});
+	}
+	public doughnutChartLabels: string[] = [];
+	public doughnutChartData: ChartData<'doughnut'> = {
+		labels: this.doughnutChartLabels,
+		datasets: [],
+	};
+	public doughnutChartType: ChartType = 'doughnut';
+
+	// events
+	public chartClicked({ event, active }: { event: MouseEvent; active: {}[] }): void {
+		console.log(event, active);
+	}
+
+	public chartHovered({ event, active }: { event: MouseEvent; active: {}[] }): void {
+		console.log(event, active);
 	}
 }
