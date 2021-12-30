93ce9ca296ec33ed71db7c99537d1cb9c33ad607
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-29 23:41:32 -0600

    Marcadores en el mapa

--- 09-mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
+++ 09-mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
-  selector: 'app-marcadores',
-  templateUrl: './marcadores.component.html',
-  styleUrls: ['./marcadores.component.css']
+	selector: 'app-marcadores',
+	templateUrl: './marcadores.component.html',
+	styleUrls: ['./marcadores.component.css'],
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+	mapa!: mapboxgl.Map;
+	@ViewChild('mapa') divMapa!: ElementRef;
 
-  constructor() { }
+	zoomLevel: number = 10;
+	center: [number, number] = [-93.10127876276135, 16.77712879407655];
 
-  ngOnInit(): void {
-  }
+	constructor() {}
+	ngAfterViewInit(): void {
+		this.mapa = new mapboxgl.Map({
+			container: this.divMapa.nativeElement,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: this.center,
+			zoom: 17,
+		});
 
+		const marker = new mapboxgl.Marker().setLngLat(this.center).addTo(this.mapa);
+	}
 }
