525d167edf9372c692d4ac8a9679bcf03dede1cf
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-30 14:49:01 -0600

    Formulario reactivo tradicional

--- 11-directivasApp/src/app/productos/pages/agregar/agregar.component.ts
+++ 11-directivasApp/src/app/productos/pages/agregar/agregar.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-agregar',
-  templateUrl: './agregar.component.html',
-  styles: [
-  ]
+	selector: 'app-agregar',
+	templateUrl: './agregar.component.html',
+	styles: [],
 })
-export class AgregarComponent implements OnInit {
+export class AgregarComponent {
+	miFormulario: FormGroup = this.fb.group({
+		nombre: ['', Validators.required],
+	});
+	constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	tieneError(campo: string) {
+		return this.miFormulario.get(campo)?.invalid || false;
+	}
 }
