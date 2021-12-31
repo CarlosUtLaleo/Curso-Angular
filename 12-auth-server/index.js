fefd7ec4f66cd6f8752f6af4cb88f457ca0e4113
Carlos Utrilla (LALEO) <94929111+CarlosUtLaleo@users.noreply.github.com>
2021-12-31 09:43:05 -0600

    Configurar servidor de Express

--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,1 +1,14 @@
-console.log('Hola desde index');
+const express = require('express');
+
+//Crear servidor de expres
+
+const app = express();
+
+//Get
+app.get('/', (req, res) => {
+	res.json({ ok: true, msg: 'Todo salio bien', uid: 1234 });
+});
+
+app.listen(4000, () => {
+	console.log(`Servidor corriendo en el puerto ${4000}`);
+});
