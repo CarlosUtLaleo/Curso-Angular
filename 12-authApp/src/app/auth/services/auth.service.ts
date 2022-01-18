2ad203aef179e2c3a37f3b0afc47f09320cd3336
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2022-01-18 10:54:32 -0600

    Mantener el usuario activo

--- 12-authApp/src/app/auth/services/auth.service.ts
+++ 12-authApp/src/app/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from "src/environments/environment";
 import { AuthResponse, Usuario } from "../interfaces/auth.interface";
 import { catchError, map, tap } from "rxjs/operators";
@@ -27,6 +27,7 @@ export class AuthService {
 		return this.http.post<AuthResponse>(url, body).pipe(
 			tap((resp) => {
 				if (resp.ok) {
+					localStorage.setItem("token", resp.token!);
 					this._usuario = {
 						name: resp.name!,
 						uid: resp.uid!,
@@ -36,5 +37,11 @@ export class AuthService {
 			map((resp) => resp.ok),
 			catchError((err) => of(err.error.msg))
 		);
+	}
+
+	validarToken() {
+		const url = `${this.baseUrl}/auth/renew`;
+		const headers = new HttpHeaders().set("x-token", localStorage.getItem("token") || "");
+		return this.http.get(url, { headers });
 	}
 }
